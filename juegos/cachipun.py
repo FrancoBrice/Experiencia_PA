02fec983a08f0c2512b0ba0befe223c0b56248ed
MaxPrietoC <163342005+MaxPrietoC@users.noreply.github.com>
2024-03-14 20:15:29 +0000

    cachipun listo

--- juegos/cachipun.py
+++ juegos/cachipun.py
@@ -1,7 +1,17 @@
+import random
 def cachipun():
     """
     Esta función representa el juego de cachipun.
     Debes pedir al usuario que elija piedra, papel o tijera, y luego comparar su elección con la de la computadora.
     La computadora debe elegir una opción al azar.
     """
-    pass
+    eleccion=int(input("Elije una opcion: piedra (1), tijera (2), Papel (3)",))
+    maquina=random.randint(1,3) 
+    print("La maquina eligio:", maquina)
+    if maquina==eleccion:
+        return "Empate"
+    elif (eleccion==1 and maquina==2) or (eleccion==3 and maquina==1) or (eleccion==2 and maquina==3):
+        return "ganaste"
+    else:
+        return "perdiste"
+print(cachipun())
