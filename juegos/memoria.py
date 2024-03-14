b89bf312bee14da24ecc3d81f495d13c8300ed84
FrancoBrice <69638903+FrancoBrice@users.noreply.github.com>
2024-03-14 13:28:47 +0000

    memoria

--- juegos/memoria.py
+++ juegos/memoria.py
@@ -1,3 +1,7 @@
+from random import randint 
+import time
+import os
+
 def memoria():
     """
     Esta función representa el juego de memoria.
@@ -5,4 +9,19 @@ def memoria():
     Luego, debes pedir al usuario que repita la secuencia.
     Se debe mostrar un mensaje si el usuario acierta o no.
     """
-    pass
+    print("Recuerda los siguientes números:")
+    number_list = [randint(0, 10) for i in range(5)]
+    print(number_list)
+
+    for number in number_list:
+        print(number)
+        time.sleep(2)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        
+    for i in range(5):
+        number_input = input("ingresa el primer numero\n")
+        if number_input == number:
+            print("Correcto!")
+        else: 
+            print("incorrecto")
+memoria()
